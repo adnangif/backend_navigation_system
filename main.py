a14b315de49c8ee69e185c6be3cb355bba1f97fb
Fahim Adnan <59190871+adnangif@users.noreply.github.com>
2024-02-19 14:08:46 +0600

    added simple code for reading simple text from image

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+import pytesseract
+
+
+
+if __name__ == '__main__':
+    img = cv2.imread("1.png")
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply threshold to convert to binary image
+    threshold_img = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+    # Pass the image through pytesseract
+    text = pytesseract.image_to_string(threshold_img)
+
+    # Print the extracted text
+    print("Extracted text: ")
+    print(text)
