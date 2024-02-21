47872b34d491a51086af7dcbcefde6488d7e60cf
Fahim Adnan <59190871+adnangif@users.noreply.github.com>
2024-02-21 19:00:37 +0600

    added image preprocessing

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
+import requests
+import easyocr
 import cv2
-import pytesseract
 
+def preprocess_image(image_path):
+    # Read the image
+    image = cv2.imread(image_path)
+
+    # Convert the image to grayscale
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Apply Gaussian blur to reduce noise
+    blurred_image = cv2.GaussianBlur(grayscale_image, (5, 5), 0)
+
+    # Apply adaptive thresholding
+    thresholded_image = cv2.adaptiveThreshold(blurred_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 4)
+
+    # Invert the thresholded image
+    inverted_image = cv2.bitwise_not(thresholded_image)
+    cv2.imwrite('output.jpg',inverted_image)
+
+    return inverted_image
+
+def download_image(url, file_name):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(file_name, 'wb') as file:
+            file.write(response.content)
+        print(f"Image downloaded successfully as '{file_name}'")
+    else:
+        print("Failed to download image")
 
 
 if __name__ == '__main__':
-    img = cv2.imread("1.png")
-    # Convert image to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    server_url = "http://192.168.0.110/capture"
+    download_image(server_url,'fetched.jpg')
 
-    # Apply threshold to convert to binary image
-    threshold_img = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    inv = preprocess_image('fetched.jpg')
 
-    cv2.imwrite("output.jpg",threshold_img)
+    reader = easyocr.Reader(['en'],gpu=False) 
+    result = reader.readtext(inv)
+    print(result)
 
-    # Pass the image through pytesseract
-    text = pytesseract.image_to_string(threshold_img)
+    
+    
 
-    # Print the extracted text
-    print("Extracted text: ")
-    print(text)
